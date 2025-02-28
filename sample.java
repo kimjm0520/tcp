8737271cf60bb68f1d2715412c9d2cfcb7efd1f0
kimjm0520 <152957770+kimjm0520@users.noreply.github.com>
2025-02-28 13:33:57 +0900

    Add files via upload

--- sample.java
+++ sample.java
@@ -18,23 +18,42 @@ public class sample{
         ServerSocket server=null;
         Socket client=null;
         try{
-            InetAddress ip = InetAddress.getByName("192.168.0.7"); //로컬 ip주소 입력
+            InetAddress ip = InetAddress.getByName("192.168.0.38"); //로컬 ip주소 입력 192.168.0.38
             int port = 9999;
             server=new ServerSocket();
             server.bind(new InetSocketAddress(ip, port));
-            while(true){
+            while(true) {
                 System.out.println("Server:Waiting for request.");
-                client=server.accept();
+                client = server.accept();
                 System.out.println("Server:Accepted.");
-                InputStream stream=client.getInputStream();
-                BufferedReader in =new BufferedReader(
+                InputStream stream = client.getInputStream();
+                BufferedReader in = new BufferedReader(
                         new InputStreamReader(stream));
-                String data=null;
-                String
+                String data = null;
+                StringBuilder receivedData = new StringBuilder();
+                while ((data = in.readLine()) != null){
+                    receivedData.append(data);
+                }
+                System.out.println("Received data:"+receivedData);
+                in.close();
+                stream.close();
+                client.close();
+                if(receivedData!=null && "EXIT".equals(receivedData.toString())){
+                    System.out.println("Stop SocketServer");
+                    break;
+                }
+                System.out.println("------");
             }
-        }
-        catch (Exception e){
-
+        } catch (Exception e){
+            e.printStackTrace();
+        }finally{
+            if(server!=null){
+                try {
+                    server.close();
+                } catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
